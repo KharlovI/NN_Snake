886c73bc3033e7fa557c289bf293045c0bedf7da
Vyacheslav <slavixg409@gmail.com>
2023-02-23 00:23:35 +0200

    added sfml as local dependency

---
+++ cmake-build-debug/CMakeFiles/AISnake.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "AISnake.exe"
+  "AISnake.exe.manifest"
+  "AISnake.pdb"
+  "CMakeFiles/AISnake.dir/Snake/Apple.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/Apple.cpp.obj.d"
+  "CMakeFiles/AISnake.dir/Snake/Button.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/Button.cpp.obj.d"
+  "CMakeFiles/AISnake.dir/Snake/Genotype.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/Genotype.cpp.obj.d"
+  "CMakeFiles/AISnake.dir/Snake/Snake.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/Snake.cpp.obj.d"
+  "CMakeFiles/AISnake.dir/Snake/Utilities.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/Utilities.cpp.obj.d"
+  "CMakeFiles/AISnake.dir/Snake/main.cpp.obj"
+  "CMakeFiles/AISnake.dir/Snake/main.cpp.obj.d"
+  "libAISnake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AISnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
